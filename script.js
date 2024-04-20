ce7b7c6f0de6303ad75f96aa3950c9c9c4aaff23
808Michael <mrgamiao@hawaii.edu>
2024-04-20 00:33:16 -1000

    Added image folder and background image for main page. Setup main page with css, html, and js functionality. Added carousel code in javascript to select island to learn about.

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+        nextArrow: '<button type="button" class="slick-next">Next</button>'
+    });
+});
