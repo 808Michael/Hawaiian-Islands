3d44b6a1a8c01d8b1ad1110c047966fd4b75b974
808Michael <mrgamiao@hawaii.edu>
2024-04-29 16:13:17 -1000

    Made major css changes to the Hawaii css page.

--- Hawaii/hawaii.js
+++ Hawaii/hawaii.js
@@ -2,10 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const titleBox = document.querySelector('.title-box');
     const title = titleBox.querySelector('h1');
 
-    titleBox.addEventListener('click', () => {
+    titleBox.addEventListener('mouseenter', () => {
         if (title.textContent.trim() === 'Hawaii') {
             title.textContent = 'The Big Island';
         } else {
+            title.textContent = 'Hawaii';
+        }
+    });
+
+    titleBox.addEventListener('mouseleave', () => {
+        if (title.textContent.trim() === 'The Big Island') {
             title.textContent = 'Hawaii';
         }
     });
