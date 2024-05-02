08edcc5b18fcb512907223a5c89e2ee31d27e201
808Michael <mrgamiao@hawaii.edu>
2024-05-01 16:24:07 -1000

    Finished Niihau page, will add Youtube video if time permits.

--- Niihau/niihau.js
+++ Niihau/niihau.js
@@ -16,3 +16,19 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const accordions = document.querySelectorAll('.accordion');
+
+    accordions.forEach(accordion => {
+        accordion.addEventListener('click', () => {
+            accordion.classList.toggle('active');
+            const panel = accordion.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + 'px';
+            }
+        });
+    });
+});
